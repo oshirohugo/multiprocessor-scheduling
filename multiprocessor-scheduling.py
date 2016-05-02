974cadbe569c6f4f079cc0941616dfa2f03fb3c2
oshirohugo <oshiro.hugo@gmail.com>
2016-05-01 23:37:30 -0300

    First commit

---
+++ multiprocessor-scheduling.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# Problem defined here http://otfried.org/courses/cs500/slides-approx.pdf
+
+from random import randint
+from random import shuffle
+
+population_sz = 10
+reproduction_rate = 0.5
+number_of_generations = 1000
+
+number_of_machines = 3  # M
+number_of_jobs = 6  # N
+
+jobs_time = [2, 3, 4, 6, 2, 2]
+
+
+# cromossome [ nofjobs_in_1, nofjobs_in_2, ... nofjobs_in_m, job1, job2, ..., jobn ]
+
+# auxiliary function
+
+def get_jobs_per_machine(individual):
+    return individual[0:number_of_machines]
+
+
+def generate_jobs_per_machine_part():
+    jobs_per_machine = []
+
+    # random generate jobs for m - 1 machines
+    machine = 0
+    limit = number_of_machines
+    number_of_allocated_jobs = 0
+    while machine < number_of_machines - 1:
+        number_of_jobs = randint(0, limit)
+        jobs_per_machine.append(number_of_jobs)
+        number_of_allocated_jobs += number_of_jobs
+        limit -= number_of_allocated_jobs
+        machine += 1
+
+    # atribute remaining jobs to last machine
+    jobs_per_machine.append(number_of_jobs - number_of_allocated_jobs)
+
+    shuffle(jobs_per_machine)
+    return jobs_per_machine
+
+
+def generate_chromossome():
+    jobs_per_machine = generate_jobs_per_machine_part()
+    jobs = jobs_time
+    shuffle(jobs)
+    return jobs_per_machine + jobs
+
+
+# problem functions
+
+def fitness(individual):
+    jobs_per_machine = get_jobs_per_machine(individual)
+
+    # Extract total amount of time in for jobs allocated in each machine
+    j = number_of_machines
+    time_per_machine = []
+    machine = 0
+    while machine < number_of_machines:
+        total_time = 0
+        job = 0
+        while job < jobs_per_machine[machine]:
+            total_time += individual[j + job]
+        j = job
+
+        time_per_machine.append(total_time)
+        machine += 1
+
+    return min(time_per_machine)
+
+
+def initial_generation(population_sz, jobs_time):
+    initial_population = []
+    individual = 0
+    while individual < population_sz:
+        initial_population.append(generate_chromossome())
+        individual += 1
+    return initial_population
+
+
+def reproduce(population, reproduction_rate):
+    return
+
+
+def mutate(population, mutation_rate):
+    return
+
+
+population = initial_generation(population_sz, jobs_time)
+
+generation = 0
+while generation < number_of_generations:
+    population = reproduce(population, reproduction_rate)
+    population = mutate(population, reproduction_rate)
+    generation += 1
+
+print list(chunks(jobs_time, 6 / 3))[0]
+print "Finish"
